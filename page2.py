4c59fb466fdc465d52c84c55b0fdfb45e53da514
UZOKMUROD <136238255+UZOKMUROD@users.noreply.github.com>
2023-12-30 16:11:20 +0500

    first_ai

---
+++ page2.py
@@ -0,0 +1,68 @@
+import streamlit as st
+st.title("Assalomu alaykum siz bu yerda dasturni asosiy kodini kurishingi mumkin")
+
+
+code = '''import streamlit as st
+from fastai.vision.all import *
+import pathlib
+import numpy as np
+temp=pathlib.PosixPath
+pathlib.PosixPath=pathlib.WindowsPath
+
+plt=platform.system()
+if plt == "Linux" : pathlib.WindowsPath=pathlib.PosixPath
+
+from st_pages import Page, Section, show_pages, add_page_title
+import streamlit as st
+
+
+
+# Either this or add_indentation() MUST be called on each page in your
+# app to add indendation in the sidebar
+add_page_title()
+
+# Specify what pages should be shown in the sidebar, and what their titles and icons
+# should be
+
+show_pages(
+    [
+        Page("main.py", "Home", "🏠"),
+        Page("page2.py", "Dastur kodi", ),
+        Page("rezume.py ","Rezume", icon="💪"),
+        
+        
+    ]
+)
+
+st.title("Assalomu alaykum siz bu yerda Bus, Tank, Train rasmlarini ajrata oladigan AI sinab ko'rishingiz mumkin va rasm yuklashda pastdagi 3 turgagi rasmlarni yuklang")
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+   st.header("Train")
+   st.image('111.webp', caption='')
+
+with col2:
+   st.header("Bus")
+   st.image("4.jfif")
+
+with col3:
+   st.header("Tank")
+   st.image("1.jfif")
+
+img=st.file_uploader("Rasm yuklash uchun joy", type=["png","jpg","jpeg","gif", "svg", "webp","jfif"])
+if img:
+    st.image(img)
+
+    imgs=PILImage.create(img)
+
+    model=load_learner("Transport.pkl")
+
+    pred, pred_id,prods=model.predict(imgs)
+
+    st.success(f"Aniqlangan rasm nomi: {pred}")
+    st.info(f"Rasm aniqliyligi : {prods[pred_id]*100:.1f}%")
+
+    
+'''
+st.code(code, language='python')
---
+++ rezume.py
@@ -0,0 +1,8 @@
+import streamlit as st
+st.title("Assalomu alaykum mening ismim Uzoqmurod 2004-yilda to'g'ilganman")
+st.info("Malumotim tugallanmagan oliy hozirda TATU ning DIF yani dasturiy injinering fakultetini 2-kurs talabasiman")
+st.success("Qiziqishlarin: AI texnalogiyalariga qiziqaman ularning ham ichidan ko'proq tasvir bilan ishlovchi AI larga qiziqaman kelajakda shu kasbni mutahasisi bo'lmoqchiman")
+st.error("Mohirdev praktikum kurslarni haqida qisqacha fikrim: Ular juda ajoyib sodda va odamni zeriktirmaydigan qilib tushuntirilgan lekin agar amaliyotlar yanada ko'paytirilsa va bizga AI modellarni yani o'zimiz kichikroq bo'lsa ham AI kodini yozishni o'rgatish qo'shilsa yaxshiroq bo'lardi deb o'ylayman")
+st.markdown("Murojat uchun")
+st.link_button("INSTAGRAM","https://www.instagram.com/uzokmurod_04/?next=%2F")
+st.link_button("TELEGRAM","https://t.me/Uzokmurod_04")
